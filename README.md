443b421e3947e4e6e5055bdbea158423dfcc8a3f
Steve Quinlan <smq100@gmail.com>
2021-05-30 08:16:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blockchain Analysis Framework
+
+This is my personal blockchain analysis framework.
+
+The state and functionality of code is variable and should not be considered production-worthy.
---
+++ server/server/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from blockchain import views
+from blockchain.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url('^get_chain$', views.get_chain, name="get_chain"),
+    url('^mine_block$', views.mine_block, name="mine_block"),
+    url('^add_transaction$', views.add_transaction, name="add_transaction"),
+    url('^is_valid$', views.is_valid, name="is_valid"),
+    url('^connect_node$', views.connect_node, name="connect_node"),
+    url('^replace_chain$', views.replace_chain, name="replace_chain"),
+]
